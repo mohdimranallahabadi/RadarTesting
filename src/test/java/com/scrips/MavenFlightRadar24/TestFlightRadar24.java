78bb1a69bee25a8ae33314c445369e321242c590
wwwim <wwwim@DESKTOP-9MKKMSB>
2021-07-10 12:43:16 +0530

    Test search changed

--- src/test/java/com/scrips/MavenFlightRadar24/TestFlightRadar24.java
+++ src/test/java/com/scrips/MavenFlightRadar24/TestFlightRadar24.java
@@ -17,7 +17,7 @@ public class TestFlightRadar24 extends BaseTest
 	{
 		POMFlightRadar radar=new POMFlightRadar(driver);
 		radar.fullScreenMethod();
-		radar.search();
+		//radar.search();
 		BasePage.scrollWebPage(1000, 200, 10);
 		radar.goToLatLonZoom(41.30, -76.82, 11.5);
 		radar.goToLatLonZoom(-76.43, 41.34, 8);
